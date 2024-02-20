5c9a6814d4010e5a24ab7bb5483afbddd0e26fcf
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-20 02:38:15 -0800

    Must be about a quarter past three.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,21 +15,27 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp, gotChanged;
 
 	i = 0;
 	while (i < size - 1)
 	{
+		gotChanged = 27;
 		j = 0;
-		while (j < size - i - 1)
+		while (j < size - 1)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp  = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				gotChanged = 28;
 			}
 			j++;
+		}
+		if (gotChanged == 27)
+		{
+			break;
 		}
 		print_array(array, size);
 		i++;
