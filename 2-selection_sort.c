55dd93b8665f57550dd3f34955ed39624a60cf59
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-20 02:13:01 -0800

    It is a big conspiracy, it is not true

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * selection_sort - this function sorts elements in an array
+ * by implementing the selection sort algorithm
+ *
+ * @array: the array to be sorted
+ * @size: the size of the array thatis going to be sorted
+ *
+ * Description: refer to the first commented out line
+ *
+ * Return: Nada
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, currentMinimum;
+
+	for (i = 0; i < size; i++)
+	{
+		currentMinimum = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[currentMinimum])
+			{
+				currentMinimum = j;
+			}
+		}
+		temp = array[i];
+		array[i] = array[currentMinimum];
+		array[currentMinimum] = temp;
+		print_array(array, size);
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+This repo will hold exercises on the implementations of the famous sorting algorithms in C.
